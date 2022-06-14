292d66e0e414c727b2ab68e0183fa99e74f7b230
Tim0fey <86416015+Tim123456Tim@users.noreply.github.com>
2022-06-14 18:21:16 +0400

    Update index.php

--- index.php
+++ index.php
@@ -3,13 +3,13 @@
     <head>
         <title>Интересные факты - fact0rial</title>
         <meta name="description" content="Здесь вы сможете находить интересные факты каждый день!">
-        <meta name="keywords" content="факты, факты каждый день, интересные факты каждый день, fact0rial, fact0rial.ru">
+        <meta name="keywords" content="факты, факты каждый день, интересные факты каждый день, fact0rial, fact0rial.ru, Fact0rial">
         <link rel="apple-touch-icon" href="images/1024.png">
-        <meta name="apple-mobile-web-app-title" content="fact0rial">
+        <meta name="apple-mobile-web-app-title" content="Fact0rial">
         <meta property="og:description" content="Здесь вы сможете находить интересные факты каждый день!">
         <meta name="twitter:description" content="Здесь вы сможете находить интересные факты каждый день!">
-        <meta property="og:title" content="Интересные факты - fact0rial">
-        <meta name="twitter:title" content="Интересные факты - fact0rial">
+        <meta property="og:title" content="Интересные факты - Fact0rial">
+        <meta name="twitter:title" content="Интересные факты - Fact0rial">
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="shortcut icon"  href="images/logo.png">
@@ -29,7 +29,7 @@
         <meta name="google-site-verification" content="qUoFWSvKnc_8gndvMiY-Pii1ajo1ZHWusNNYbbVCNfk"/>
     </head>
 	<body>
-		<h1 style="text-align: center;">fact0rial</h1>
+		<h1 style="text-align: center;">Fact0rial</h1>
 		<hr style="max-width: 450px; min-width: 140px;">
 		<div class = "about">
 			<h2>Здесь вы найдёте самые интересные и конечно же только проверенные факты</h2>
